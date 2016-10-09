dd206f4af29616bb2a1a758981ba4b78c1cf3460
Sara Gasperetti <saragasperetti@gmail.com>
2016-10-09 10:49:26 +0200

    finished lab02

--- lab02/myworkspace/HealthProfileReader/src/HealthProfileReader.java
+++ lab02/myworkspace/HealthProfileReader/src/HealthProfileReader.java
@@ -27,74 +27,41 @@ public class HealthProfileReader {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		/* Solution to Exercise #01-2e */
-		int argCount = args.length;
-		if (argCount == 0) {
-			System.out
-					.println("I cannot create people out of thing air. Give me at least a name and lastname.");
-		} else if (argCount < 2) {
-			System.out
-					.println("Are you sure you gave me ALL the information I need?");
-		} else {
-			String method = args[0];
-			if (method.equals("createNewPerson")) {
-				Long personId = Long.parseLong(args[1]);
-				String firstname = args[2];
-				String lastname = args[3];
-				String birthdate = args[4];
-				createPerson(personId, firstname, lastname, birthdate);
-			} else if (method.equals("displayHealthProfile")) {
-				Long personId = Long.parseLong(args[1]);
-				displayHealthProfile(personId);				
-			}  else if (method.equals("updateHealthProfile")) {
-				Long personId = Long.parseLong(args[1]);
-				Double height = Double.parseDouble(args[2]);
-				Double weight = Double.parseDouble(args[3]);
-				updateHealthProfile(personId, height, weight);
-			} else if (method.equals("displayBMI")){
-				Long personId = Long.parseLong(args[1]);
-				displayBMI(personId);		
-			} else {
-				System.out.println("The system did not find the method '"+method+"'");
-			}
-		}
-	}
 
 	/* Solution to Exercise #01-2b */
-	private static void createPerson(Long personId, String firstname,
+	public String createPerson(Long personId, String firstname,
 			String lastname, String birthdate) {
 		Person p = new Person(personId, firstname, lastname, birthdate);
 		database.put(p.getPersonId(), p);
-		System.out.println("A new person record (" + p.getPersonId()
+		return "A new person record (" + p.getPersonId()
 				+ ") has been created for " + p.getLastname() + ", "
-				+ p.getFirstname() + " born on " + p.getBirthdate());
+				+ p.getFirstname() + " born on " + p.getBirthdate();
 	}
 
 	/* Solution to Exercise #01-2c */
-	private static void displayHealthProfile(Long personId) {
+	public String displayHealthProfile(Long personId) {
 		Person p = database.get(personId);
 		HealthProfile hp = p.gethProfile();
-		System.out.println(p.getFirstname() + " has a weight of "
-				+ hp.getWeight() + " Kg. and a height of " + hp.getHeight());
+		return p.getFirstname() + " has a weight of "
+				+ hp.getWeight() + " Kg. and a height of " + hp.getHeight();
 	}
 
 	/* Solution to Exercise #01-2d */
-	private static void updateHealthProfile(Long personId, Double height,
+	public String updateHealthProfile(Long personId, Double height,
 			Double weight) {
 		Person p = database.get(personId);
 		HealthProfile hp = p.gethProfile();
 		hp.setHeight(height);
 		hp.setWeight(weight);
-		System.out.println(p.getFirstname() + " has updated weight to "
+		return p.getFirstname() + " has updated weight to "
 				+ hp.getWeight() + " Kg. and updated height to "
-				+ hp.getHeight());
+				+ hp.getHeight();
 	}
 	
-	private static void displayBMI(Long personId) {
+	public String displayBMI(Long personId) {
 		Person p = database.get(personId);
 		HealthProfile hp = p.gethProfile();
-		System.out.println(p.getFirstname() + "'s BMI is " + hp.getBMI());
+		return p.getFirstname() + "'s BMI is " + hp.getBMI();
 	}
 
 
